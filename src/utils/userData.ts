f5a0e0bd240f63ada9a0c134b8d2ee55784d7b75
naymi <naymi.prestes1997@gmail.com>
2023-02-12 13:28:42 -0400

    desenvolvimento do portfolio

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Naymi Prestes",
+  githubUser: "naymiprestes",
+  linkedinUser: "naymi-prestes-87a61896",
+  whatsappNumber: "+5592992684279",
+  emailUser: "naymi.prestes1997@gmail.com",
+  instagramUser: "",
   facebookUser: "",
 };
